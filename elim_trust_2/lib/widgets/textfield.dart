2ca06381a40d44e885931c3d6a3b357f6d28e880
Yvonne <wanguiy001@gmail.com>
2025-05-28 10:25:58 +0300

    addeed new textfield widget and modified profile page widget

---
+++ elim_trust_2/lib/widgets/textfield.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class Mytextfield extends StatefulWidget {
+  final TextEditingController? controller;
+
+   const Mytextfield({super.key,
+   this.controller,
+   this.labelText, 
+   this.helperText, 
+   this.hintText,
+    this.errorText,
+     this.obscureText,
+      this.keyboardType,
+       this.textInputAction});
+  final String? labelText;
+  final String? helperText;
+  final String? hintText;
+  final String? errorText;
+  final bool? obscureText;
+  final TextInputType? keyboardType;
+  final TextInputAction? textInputAction;
+
+  @override
+  State<Mytextfield> createState() => _MytextfieldState();
+}
+
+class _MytextfieldState extends State<Mytextfield> {
+  late FocusNode _focusNode;
+  bool _isFocused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_onFocusChange);
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    setState(() {
+      _isFocused = _focusNode.hasFocus;
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0), // Match TextField's border radius
+        boxShadow: _isFocused
+            ? [
+
+                // BoxShadow(
+                //   color: Colors.blue.withOpacity(0.5), // Shadow color when focused
+                //   spreadRadius: 2,
+                //   blurRadius: 5,
+                //   offset: Offset(0, 3), // Changes position of shadow
+                // ),
+                // BoxShadow(
+                //   color: Colors.blue.withOpacity(0.3), // Lighter shadow for depth
+                //   spreadRadius: 1,
+                //   blurRadius: 3,
+                //   offset: Offset(0, 1), // Changes position of shadow
+                // ),
+                
+              ]
+            : [],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0), // Keep padding for inner TextField
+        child: TextField(
+          controller: widget.controller,
+          focusNode: _focusNode,
+          decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0), 
+              borderSide: BorderSide(color: Colors.grey, width: 2.0),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0), 
+              borderSide: BorderSide(color: Colors.blue, width: 2.0),
+            ),
+            
+            labelText: widget.labelText,
+            labelStyle: TextStyle(color: _isFocused ? Colors.blue : Colors.black),
+            helperText: widget.helperText,
+            fillColor: _isFocused ? Colors.white : const Color.fromARGB(255, 139, 187, 227),
+            filled: true,
+            hintText: widget.hintText,
+            hintStyle: TextStyle(color: Colors.grey),
+            errorText: widget.errorText,
+            errorStyle: TextStyle(color: Colors.red),
+          ),
+          style: TextStyle(color: Colors.black),    
+          cursorColor: Colors.blue,
+          cursorHeight: 20,
+          cursorWidth: 2,
+          textAlign: TextAlign.start,
+          textAlignVertical: TextAlignVertical.center,
+          maxLines: 1,
+          keyboardType: widget.keyboardType,
+          textInputAction: widget.textInputAction,
+          obscureText: widget.obscureText ?? false,
+          obscuringCharacter: '*',
+          autocorrect: true,
+          enableSuggestions: true,  
+        ),
+      ),
+    );
+  }
+}
