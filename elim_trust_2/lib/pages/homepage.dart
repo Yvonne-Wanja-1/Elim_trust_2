a7800b8c2bc91c9315b759eb799e10e034446e52
Yvonne <wanguiy001@gmail.com>
2025-06-04 09:59:28 +0300

    changed the positioning of the image withim the news section

--- elim_trust_2/lib/pages/homepage.dart
+++ elim_trust_2/lib/pages/homepage.dart
@@ -1,6 +1,4 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-
-import 'package:elim_trust_2/pages/yprep.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -21,50 +19,48 @@ class _ImpactCardData {
 class _ImpactCardWidget extends StatelessWidget {
   final _ImpactCardData data;
   final double? cardWidth;
-  final VoidCallback? onCardTap; // Renamed for clarity if you want to keep whole card tap
-  final VoidCallback? onImageTap; // New callback for image tap
+  final VoidCallback? onCardTap;
+  final VoidCallback? onImageTap;
 
   const _ImpactCardWidget({
     required this.data,
     this.cardWidth,
     this.onCardTap,
-    this.onImageTap, // Added to constructor
+    this.onImageTap,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: onCardTap, // Use the renamed callback for the whole card
-      borderRadius: BorderRadius.circular(15.0), // Optional: for ripple effect to match card shape
+      onTap: onCardTap,
+      borderRadius: BorderRadius.circular(15.0),
       child: SizedBox(
         width: cardWidth ?? 300.0,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            // Wrap the Image with GestureDetector or InkWell for tap detection
-            GestureDetector( 
-              onTap: onImageTap, // Use the onImageTap callback passed to the widget
+            GestureDetector(
+              onTap: onImageTap,
               child: Container(
                 height: 300,
-                width: 300.0, // Explicitly set width to match height
+                width: 300.0,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15.0),
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.blue.withOpacity(0.5), // Adjust the color and opacity as needed
+                      color: Colors.blue.withOpacity(0.5),
                       spreadRadius: 2,
                       blurRadius: 5,
-                      offset: const Offset(0, 3), // changes position of shadow
-                      ),
+                      offset: const Offset(0, 3),
+                    ),
                   ],
                 ),
                 clipBehavior: Clip.antiAlias,
                 child: Image.asset(data.imagePath, fit: BoxFit.cover),
               ),
             ),
-            // Wrap the text Column with a GestureDetector
             GestureDetector(
-              onTap: onImageTap, // Reuse the onImageTap for the text section as well
+              onTap: onImageTap,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -100,16 +96,14 @@ class _ImpactCardWidget extends StatelessWidget {
                       textAlign: TextAlign.start,
                       maxLines: 5,
                       overflow: TextOverflow.ellipsis,
-                      ),
                     ),
+                  ),
                 ],
               ),
             ),
-                
           ],
         ),
       ),
-
     );
   }
 }
@@ -118,9 +112,7 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   static final List<_ImpactCardData> _impactCardItems = [
-    
     const _ImpactCardData(
-      
       imagePath: 'images/t.png',
       title: 'Y-PREP',
       description:
@@ -147,7 +139,7 @@ class HomePage extends StatefulWidget {
   ];
 
   @override
-  _HomePageState createState() => _HomePageState(); 
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
@@ -166,20 +158,20 @@ class _HomePageState extends State<HomePage> {
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
                   decoration: BoxDecoration(
-                    color: const Color(0xFFF7F7F7), // Moved color here
+                    color: const Color(0xFFF7F7F7),
                     borderRadius: BorderRadius.circular(30),
                     boxShadow: [
                       BoxShadow(
                         color: Colors.blue,
                         spreadRadius: 2,
                         blurRadius: 5,
-                        offset: const Offset(0, 3), // changes position of shadow
+                        offset: const Offset(0, 3),
                       ),
                     ],
                   ),
                   height: 250,
                   child: Image.asset(
-                    'images/elimtrust.png',  
+                    'images/elimtrust.png',
                     fit: BoxFit.contain,
                     alignment: Alignment.topCenter,
                   ),
@@ -206,64 +198,31 @@ class _HomePageState extends State<HomePage> {
                   children: HomePage._impactCardItems.asMap().entries.map((entry) {
                     int idx = entry.key;
                     _ImpactCardData data = entry.value;
-                    double? customWidth;
-                    VoidCallback? imageSpecificOnTap; // This will be for the image tap
+                    VoidCallback? imageSpecificOnTap;
 
-
-                    // VoidCallback? wholeCardTap; // If you need a separate tap for the whole card
-                    // Define the action for when the Y-PREP image is tapped
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
                     if (data.title == 'Y-PREP') {
                       imageSpecificOnTap = () {
                         Navigator.pushNamed(context, '/yprep');
-
-                        // This will navigate to the Y-PREP page when the image is tapped
-
-
-                        
                       };
-                    }
-                    // You can add more `else if` blocks here for other cards
-                    // For example, if you want to add a specific action for the 'Mats Dialogue' image tap:
-                    else if (data.title == 'Mats Dialogue') {
+                    } else if (data.title == 'Mats Dialogue') {
                       imageSpecificOnTap = () {
-                        Navigator.pushNamed(context, '/matsdialogue');// this paragraph means  that 
+                        Navigator.pushNamed(context, '/matsdialogue');
                       };
-                    }
-
-
-
-                    else if (data.title == 'Vunja Kalabash') {
+                    } else if (data.title == 'Vunja Kalabash') {
                       imageSpecificOnTap = () {
-                        // Navigate to the Vunja Kalabash page
                         Navigator.pushNamed(context, '/vunja');
                       };
                     } else if (data.title == 'Capacity Building of Spiritual & Community Leaders') {
                       imageSpecificOnTap = () {
-                        // Navigate to the Capacity Building page
                         Navigator.pushNamed(context, '/capacity');
                       };
-                    } 
-                    
-                    // if their images should also be tappable with different actions.
+                    }
 
                     return Padding(
                       padding: EdgeInsets.only(right: idx == HomePage._impactCardItems.length - 1 ? 0 : 16.0),
                       child: _ImpactCardWidget(
-                        data: data, 
-                        cardWidth: customWidth, 
-                        onImageTap: imageSpecificOnTap, // Pass the image-specific tap action here
-                        // onCardTap: wholeCardTap, // Use this if the whole card should also be tappable
+                        data: data,
+                        onImageTap: imageSpecificOnTap,
                       ),
                     );
                   }).toList(),
@@ -306,53 +265,49 @@ class _HomePageState extends State<HomePage> {
                   color: const Color.fromARGB(255, 175, 211, 240),
                   borderRadius: BorderRadius.circular(30),
                 ),
-                child: Row(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.only(left: 10),
-                            child: Text(
-                              "🌍 UN H6 Joint Programme on RMNCAH (2015–2020):",
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                decorationColor: Colors.blue,
-                                decorationThickness: 2,
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue,
-                              ),
-                            ),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(left: 25, right: 25, top: 5),
-                            child: Text(
-                              '''
-                In Mandera, we worked with adolescents and young mothers to advance reproductive and maternal health under this multi-agency initiative.''',
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                                fontFamily: 'Arial',
-                                fontStyle: FontStyle.italic,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
                     Container(
                       height: 250,
-                      width: 250,
-                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 16, right: 5),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(50),
-                          child: Image.asset('images/UN.jpg', fit: BoxFit.cover),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Image.asset('images/UN.jpg', fit: BoxFit.cover),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Text(
+                        "🌍 UN H6 Joint Programme on RMNCAH (2015–2020):",
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.blue,
+                          decorationThickness: 2,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
                         ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+                      child: Text(
+                        '''
+In Mandera, we worked with adolescents and young mothers to advance reproductive and maternal health under this multi-agency initiative.''',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'Arial',
+                          fontStyle: FontStyle.italic,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
                     ),
                   ],
@@ -419,7 +374,7 @@ class _HomePageState extends State<HomePage> {
                             padding: EdgeInsets.symmetric(horizontal: 25),
                             child: Text(
                               '''
-                Some Gender Based Violence acts are perpetuated by Harmful Traditional Practices (HTP). Harmful traditional practices like Female Genital Mutilation (FGM) , early and forced marriage are still being practiced by several communities in Africa and across the world. These backward and barbaric...''',
+Some Gender Based Violence acts are perpetuated by Harmful Traditional Practices (HTP). Harmful traditional practices like Female Genital Mutilation (FGM), early and forced marriage are still being practiced by several communities in Africa and across the world. These backward and barbaric...''',
                               style: TextStyle(
                                 fontSize: 14,
                                 fontWeight: FontWeight.w400,
