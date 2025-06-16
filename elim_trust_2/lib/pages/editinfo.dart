cf42d82ccc68eed6a0f9c67f285525b84e411e88
Yvonne Wangui <wanguiyvonne333@gmail.com>
2025-06-16 14:48:14 +0300

    added if statement to check error inputs

--- elim_trust_2/lib/pages/editinfo.dart
+++ elim_trust_2/lib/pages/editinfo.dart
@@ -3,8 +3,87 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class EditInfo extends StatelessWidget {
+class EditInfo extends StatefulWidget {
   const EditInfo({super.key});
+
+  @override
+  State<EditInfo> createState() => _EditInfoState();
+}
+
+class _EditInfoState extends State<EditInfo> {
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  String? _firstNameError;
+  String? _lastNameError;
+  String? _emailError;
+  String? _phoneError;
+
+  @override
+  void initState() {
+    super.initState();
+    _firstNameController.addListener(_validateFirstName);
+    _lastNameController.addListener(_validateLastName);
+    _emailController.addListener(_validateEmail);
+    _phoneController.addListener(_validatePhoneNumber);
+  }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _phoneController.dispose();
+    super.dispose();
+  }
+
+  void _validateFirstName() {
+    setState(() {
+      if (_firstNameController.text.isEmpty) {
+        _firstNameError = 'Please enter your first name';
+      } else {
+        _firstNameError = null;
+      }
+    });
+  }
+
+  void _validateLastName() {
+    setState(() {
+      if (_lastNameController.text.isEmpty) {
+        _lastNameError = 'Please enter your last name';
+      } else {
+        _lastNameError = null;
+      }
+    });
+  }
+
+  void _validateEmail() {
+    setState(() {
+      final email = _emailController.text;
+      if (email.isEmpty) {
+        _emailError = 'Please enter your email address';
+      } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(email)) {
+        _emailError = 'Please enter a valid email address';
+      } else {
+        _emailError = null;
+      }
+    });
+  }
+
+  void _validatePhoneNumber() {
+    setState(() {
+      if (_phoneController.text.isEmpty) {
+        _phoneError = 'Please enter your phone number';
+      } else if (!RegExp(r'^\+?[0-9]{10,}$').hasMatch(_phoneController.text)) {
+        _phoneError = 'Please enter a valid phone number';
+      }
+      else {
+        _phoneError = null;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,11 +127,11 @@ class EditInfo extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                  Mytextfield(
-                    controller: TextEditingController(),
+                    controller: _firstNameController,
                     labelText: 'First Name',
                     helperText: 'Enter your first name',
                     hintText: 'e.g. John',
-                    errorText: 'Please enter a valid name',
+                    errorText: _firstNameError,
                     obscureText: false,
                     keyboardType: TextInputType.name,
                     textInputAction: TextInputAction.next,
@@ -60,11 +139,11 @@ class EditInfo extends StatelessWidget {
                   //last nemae:
                   const SizedBox(height: 20),
                   Mytextfield(
-                    controller: TextEditingController(),
+                    controller: _lastNameController,
                     labelText: 'Last Name',
                     helperText: 'Enter your last name',
                     hintText: 'e.g. Doe',
-                    errorText: 'Please enter a valid name',
+                    errorText: _lastNameError,
                     obscureText: false,
                     keyboardType: TextInputType.name,
                     textInputAction: TextInputAction.next,
@@ -72,11 +151,11 @@ class EditInfo extends StatelessWidget {
                   const SizedBox(height: 20),
                   //email:
                   Mytextfield(
-                    controller: TextEditingController(),
+                    controller: _emailController,
                     labelText: 'Email',
                     helperText: 'Enter your email address',
                     hintText: 'e.g. john.doe@example.com',
-                    errorText: 'Please enter a valid email',
+                    errorText: _emailError,
                     obscureText: false,
                     keyboardType: TextInputType.emailAddress,
                     textInputAction: TextInputAction.done,
@@ -84,11 +163,11 @@ class EditInfo extends StatelessWidget {
                   const SizedBox(height: 20),
                   //phone number:
                   Mytextfield(
-                    controller: TextEditingController(),
+                    controller: _phoneController,
                     labelText: 'Phone Number',
                     helperText: 'Enter your phone number',
                     hintText: 'e.g. +254712345678',
-                    errorText: 'Please enter a valid phone number',
+                    errorText: _phoneError,
                     obscureText: false,
                     keyboardType: TextInputType.phone,
                     textInputAction: TextInputAction.done,
